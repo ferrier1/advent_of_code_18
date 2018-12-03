66258636549cf45cf9dad46ee6e3843e3423b2d5
johnnydrama92 <james.ferrier01@gmail.com>
2018-12-03 22:50:51 +0000

    part 2 done

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,25 +1,50 @@
+from collections import Counter
+import itertools
+
 
 def readin(file):
     with open(file) as f:
         changes = f.read().splitlines()
         return changes
 
-def solver(list_of_changes):
-    counter = 0
+def solver(counter, list_of_changes):
+    frequencies = []
     for change in list_of_changes:
         if change[0] == '+':
             counter += int(change[1:])
+            frequencies.append(counter)
         elif change[0] == '-':
             counter -= int(change[1:])
+            frequencies.append(counter)
         else:
             break
-    print(counter)
+    return frequencies
 
 
+def calibrator(start_list, input):
+    duplicate_list = start_list
+    ans = start_list
+    while True:
+        ans = solver(ans[-1], input)
+        for num in ans:
+            if num not in duplicate_list:
+                duplicate_list.append(num)
+                print(num)
+            else:
+                return num
 
 
 def main():
-    solver(readin('inputs/input.txt'))
+    input = readin('inputs/input.txt')
+    #input = ["+3", "+3", "+4", "-2", "-4"]
+    ans = solver(0, input)
+    print("part 1 answer is {}".format(ans[-1]))
+
+
+    print(calibrator(ans, input))
+
+
+
 
 
 if __name__ == "__main__":
