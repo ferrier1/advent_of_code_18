0cebaef39d5bbbe5d42f6dc7bb9e4ca416232cce
johnnydrama92 <james.ferrier01@gmail.com>
2018-12-08 22:04:27 +0000

    part 2 answered but sketchy

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -7,7 +7,11 @@ def readin(file):
 
 
 def solver(claims):
-    used_inches = defaultdict(list)
+    used_inches = defaultdict(set)
+    bad_claim_numbers = set()
+    good_claims = {}
+    bad_claims = {}
+    bad_claims_reverse = {}
     duplicates = 0
     for claim in claims:
         parse = claim.split(" ")
@@ -16,10 +20,22 @@ def solver(claims):
         for y in range(int(size[0])):
             for x in range(int(size[1])):
                 inch = (int(top_left_corner[0]) + y, int(top_left_corner[1]) + x)
-                used_inches[inch].append(parse[0])
-    for item in used_inches:
-        if len(used_inches[item]) >= 2:
+                used_inches[inch].add(parse[0])
+    for k, v in used_inches.items():
+        if len(used_inches[k]) >= 2:
             duplicates += 1
+            for claims in used_inches[k]:
+                bad_claim_numbers.add(claims)
+    for k, v in used_inches.items():
+        for claim in bad_claim_numbers:
+            if claim in used_inches[k]:
+                bad_claims[k] = v
+    for k, v  in bad_claims.items():
+        bad_claims[k] = ''.join(v)
+    for k, v  in used_inches.items():
+        used_inches[k] = ''.join(v)
+    good_claims = set(used_inches.values()) - set(bad_claims.values())
+    print(good_claims)
     return duplicates
 
 
