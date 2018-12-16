cf3090243c5533055efd80f5f292fa3bf3a1f99e
johnnydrama92 <james.ferrier01@gmail.com>
2018-12-16 22:10:55 +0000

    simpler input

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -6,27 +6,50 @@ def readin(file):
         return input
 
 
+
+
 def taxicab_dist(a, b):
     return abs(int(a[0]) - int(b[0])) + abs(int(a[1]) - int(b[1]))
 
 
 def distances_between_points(input):
     distances = {}
+
     for combination in combinations(input, 2):
-        distances[combination] = taxicab_dist(combination[0].replace(" ", "").split(','), combination[1].replace(" ", "").split(','))
+        distances[str(combination)] = taxicab_dist(combination[0], combination[1])
     return distances
+
+
+def graph_sizer(input):
+    max_x = 0
+    max_y = 0
+    for x in input:
+        if x[0] == 0 or x[1] == 0:
+            return True
+        elif x[0] > max_x or x[1] > max_y:
+            return True
+
+
+
+
+
+
+
 
 
 def main():
     #input = readin('inputs/input.txt')
-    input = """1, 1
+    input = []
+    tmp_input = """1, 1
 1, 6
 8, 3
 3, 4
 5, 5
 8, 9""".splitlines()
-    print(distances_between_points(input))
-
+    for item in tmp_input:
+        input.append(item.replace(" ", "").split(','))
+    #print(distances_between_points(input))
+    graph_sizer(input)
 
 
 
