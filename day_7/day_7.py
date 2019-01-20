e433530f73632ddf958e23478573f7e23c1e0f31
johnnydrama92 <james.ferrier01@gmail.com>
2019-01-20 21:30:38 +0000

    some functions written

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -6,15 +6,48 @@ def readin(file):
         return input
 
 
+def parser(input):
+    a, b = [],[]
+    for step in input:
+        a.append(step[5])
+        b.append(step[36])
+    return a, b
+
+
+def start(input):
+    start_candidates = []
+    data = parser(input)
+    for letter in data[0]:
+        if letter not in data[1]:
+            start_candidates.append(letter)
+    return sorted(start_candidates)[0]
+    
+
+def solver(input):
+    data = parser(input)
+    potential_next_steps = []
+    completed_steps = []
+    completed_steps.append(start(input))
+    idx = 0
+    for constraint in data[0]:
+        print(constraint, data[1][idx])
+        if constraint in completed_steps:
+             potential_next_steps.append(data[1][idx])
+        idx += 1
+    print(potential_next_steps)
+    
 
 
 
+
+    
 
 
 
 def main():
     input = readin('inputs/input1.txt')
-    print(input)
+    solver(input)
+    
 
 
 
